48ca34c7bb1363dc6408d3429a4a29c69976c219
Dmitry A. Grechka <dmitry.grechka@gmail.com>
2015-01-21 18:53:17 +0300

    the version that is logically written but does not work

--- FetchClimate.R
+++ FetchClimate.R
@@ -1,41 +1,173 @@
 library(RCurl)
 library(rjson)
 
+
+
 fetchclimate <- function(
-                         data=NULL,
-                         lat,
-                         lon,
-                         variable,
-                         firstYear=1961,
-                         lastYear=1990,
-                         firstDay=1,
-                         lastDay=365,
-                         startHour=0,
-                         stopHour=24,
-                         url="http://fetchclimate2.cloudapp.net/",
-                         ) {
+  data=NULL,
+  lat,
+  lon,
+  envVariable,
+  firstYear=1961,
+  lastYear=1990,
+  firstDay=1,
+  lastDay=365,
+  startHour=0,
+  stopHour=24,
+  url="http://fetchclimate2.cloudapp.net/"
+) {
+  fetchCore <- function(jsonRequest,url) {
+    h = basicTextGatherer()
+    h$reset()
+    curlPerform(url = paste(url,"/api/compute"),
+                httpheader=c(Accept="text/plain", 'Content-Type' = "application/json"),
+                postfields=jsonRequest,
+                writefunction = h$update)
+    reply = h$value()
+    print(reply)
+    
+    ## wait while processing in progress
+    while (substr(reply,1,7)=='pending' || substr(reply,1,8)=='progress') {
+      Sys.sleep(1)
+      hash=strsplit(reply,"hash=")[[1]][2]
+      h$reset()
+      curlPerform(url = paste(url,"/api/status?hash=",curlEscape(hash),sep=""),
+                  httpheader=c(Accept="text/plain"),
+                  writefunction = h$update)
+      reply = h$value()
+      print(reply)
+    }
+    
+    result <- c()
+    
+    ## get result data
+    if (substr(reply,1,9)=='completed') {
+      msds = substr(reply,11,nchar(reply))
+      h$reset()
+      curlPerform(url = paste(url,"/jsproxy/data?uri=",curlEscape(msds),"&variables=values",sep=""),
+                  httpheader=c(Accept="application/json"),
+                  writefunction = h$update)
+      print(h$value())
+      result=fromJSON(h$value())
+    }
+    return(result);
+  }
+  
+  formTimeSliceRequestBody <- function(envVar,
+                                       lat,lon,
+                                       firstYear,lastYear,
+                                       firstDay,lastDay,
+                                       startHour,stopHour) {
+    ## JSON request object. 
+    ## The text may be downloaded from http://fetchclimate2.cloudapp.net/form
+    
+    timeRegion <- list(
+      Years=c(firstYear,lastYear),
+      Days=c(firstDay,lastDay),
+      Hours=c(startHour,stopHour),
+      IsIntervalsGridYears=T,
+      IsIntervalsGridDays=T,
+      IsIntervalsGridHours=T
+    )
+    
+    
+    domain <- list(
+      Lats=lat,
+      Lats2=NULL,
+      Lons=lon,
+      Lons2=NULL,
+      TimeRegion=timeRegion,
+      Mask=NULL,
+      SpatialRegionType="Points"
+    )    
+    
+    request <- list(
+      EnvironmentVariableName=envVariable,
+      ParticularDataSources="", # any availalbe
+      Domain=domain
+    )
+    
+    return(toJSON(request))
+  }
+  
   requestDataList <- list();
   
-  namesToCheck <- c("lat","lon","firstYear","lastYear","firstDay","lastDay","startHour","stopHour")        
+  vectorOrConstNames <- c("lat","lon","firstYear","lastYear","firstDay","lastDay","startHour","stopHour")        
+  
+  #first pass: setting vector values
+  for(name in vectorOrConstNames) { 
+    v <- get(name)
+    
+    if(length(v)>1) {
+      if(is.numeric(v)) {requestDataList[[name]] <-v} #explicit vector is set
+      else {
+        stop(paste("You've set non-numeric vector as a argument for ",name,". please set the numeric one"));
+      }
+    }
+    else {
+      if(is.character(v)) { # the length is 1 here
+        if(is.null(data)) {
+          stop(paste("You specified ",v," as a data frame name for ",name," but data parameter is set to NULL. Please set data parameter as well"))
+        }
+        else
+        {
+          if((v %in% names(data)) && (length(data[[v]])>1)) {
+            requestDataList[[name]] <- data[[v]]
+          }
+          else {
+            stop(paste("You spacified ",v," as a column name in data.frame. But this column is not found"))
+          }
+        }
+      }      
+    }    
+  }
   
   requestData <- as.data.frame(requestDataList)
   
-  for(name in namesToCheck) {
-    v <- get(name)
-    
-    if(length(v)>1) {requestDataList[[v]] <-v} #explicit vector is set
-    
-    if(is.null(data)) {      
-    }
-    else
-    {
-      if((v %in% names(data)) && (length(data[[v]])>1)) {requestDataList[[name]] <- data[[v]]}
-    }
+  for(name in vectorOrConstNames) { # second pass: setting single values
     if(!(name %in% names(requestData)) && (length(get(name))==1) && (is.numeric(get(name)))) {
       requestData[[name]] <- rep(get(name),nrow(requestData))
     }
   }
   
-  #now 
-  requestData
+  if(ncol(requestData) != length(vectorOrConstNames)) {
+    stop("Not all parameters are set")
+  }
+  
+  uniqueTimes <- unique(subset(requestData,select=c("firstYear","lastYear","firstDay","lastDay","startHour","stopHour")))
+  result <- vector(length=nrow(requestData))
+  for(i in 1:nrow(uniqueTimes)) {
+    r <- uniqueTimes[1,]
+    rFirstYear <- r$firstYear
+    rLastYear <- r$lastYear
+    rFirstDay <- r$firstDay
+    rLastDay <- r$lastDay
+    rStartHour <- r$startHour
+    rStopHour <- r$stopHour
+    
+    print(paste("Fetching data for time slice ",as.character(i)," out of ",nrow(uniqueTimes)))
+    
+    timeMatchMap <- requestData$firstYear==rFirstYear &
+      requestData$lastYear==rLastYear &
+      requestData$firstDay==rFirstDay &
+      requestData$lastDay==rLastDay &
+      requestData$startHour==rStartHour &
+      requestData$stopHour==rStopHour
+    
+    currentTimeSliceData <- requestData[timeMatchMap,]
+    
+    lats <- currentTimeSliceData$lat
+    lons <- currentTimeSliceData$lon
+    
+    requestJson <- formTimeSliceRequestBody(envVariable,
+                                            lats,lons,
+                                            rFirstYear,rLastYear,
+                                            rFirstDay,rLastDay,
+                                            rStartHour,rStopHour)
+    
+    subresult <- fetchCore(requestJson,url)$values
+    result[timeMatchMap] <- subresult
+  }
+  
+  return(result)
 }
